78bf8a034666e93adddfc342be684f858b2f4fb4
CrazyPenguin655 <131907411+CrazyPenguin655@users.noreply.github.com>
2026-02-14 12:07:32 +0100

    change post-show

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ function checkforOther(option) {
     switch(option) {
         case "sonstiges":
             document.getElementById("description_label").innerHTML = "Beschreibung/Liedname:";
-            document.getElementById("form_other").style.display = "flex";
+            document.getElementById("form_other_div").style.display = "flex";
             break;
         case "karaoke":
             document.getElementById("description_label").innerHTML = "Liedname:";
-            document.getElementById("form_other").style.display = "none";
+            document.getElementById("form_other_div").style.display = "none";
             break;
         case "kostumperformance":
             document.getElementById("description_label").innerHTML = "Liedname:";
-            document.getElementById("form_other").style.display = "none";
+            document.getElementById("form_other_div").style.display = "none";
             break;
         case "tanz":
             document.getElementById("description_label").innerHTML = "Liedname:";
-            document.getElementById("form_other").style.display = "none";
+            document.getElementById("form_other_div").style.display = "none";
             break;
     }
 }
@@ -26,15 +26,9 @@ function send() {
     formData.append("other", document.getElementById("form_other").value);
     formData.append("name", document.getElementById("name").value);
     formData.append("actdescription", document.getElementById("actdescription").value);
+
     fetch("https://technik.esgf.de/api/addPerformance.php", {
         method: "POST",
         body: formData
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
-    .catch(error => {
-        console.error("Error:", error);
-    });
 }
